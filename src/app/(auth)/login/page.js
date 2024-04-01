8b5181808367aa4729cd1cf5e3cad84cdd515880
zhangwei <zw900808@gmail.com>
2024-04-01 16:52:46 +0800

    update

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import {Button} from "antd";
-
+import LoginBtn from '@/components/auth/login-btn'
 export default function Login() {
     return (
         <main className="p-24">
            login page
-            <Button>注册</Button>
+            <LoginBtn/>
         </main>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      main page
-    </main>
-  );
+    const { data } = useSession()
+    const { accessToken } = data
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            main page
+            <div>Access Token: {accessToken}</div>
+        </main>
+    );
 }
