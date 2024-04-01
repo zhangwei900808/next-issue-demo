da66c2c7486a39cb5342b626c42da6d758d1b62c
zhangwei <zw900808@gmail.com>
2024-04-01 15:53:30 +0800

    update

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import {Button} from "antd";
 
 export default function Login() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="p-24">
            login page
+            <Button>注册</Button>
         </main>
     );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from 'next/link'
+import StyledComponentsRegistry from "./AntdRegistry";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div>
+        <div className='flex p-24' >
+            <Link href={`/login`}>登录</Link>
+        </div>
+      </div>
+      <div>
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </div>
+      </body>
     </html>
   );
 }
