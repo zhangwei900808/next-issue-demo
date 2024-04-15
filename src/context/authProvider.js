2c8e5c23f539fa58fa2e810cd04a221447b6117a
zhangwei <zw900808@gmail.com>
2024-04-15 18:03:39 +0800

    update

--- src/context/authProvider.js
+++ src/context/authProvider.js
@@ -6,7 +6,9 @@ export default function AuthProvider({ children }) {
     return (
         <SessionProvider // Re-fetch session every 5 minutes
             //https://next-auth.js.org/getting-started/client#options
+            //https://next-auth.js.org/getting-started/client#refetching-the-session
             refetchInterval={5 * 60}
+            refetchWhenOffline={false}
             // Re-fetches session when window is focused
             refetchOnWindowFocus={true}>
             {children}
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -49,6 +49,8 @@ export const authOptions = {
                 // 注意：这里我们在login页面已经做好了登录验证工作，然后通过singin传递到这个页面把用户信息放入到session中，
                 // 目的：为了解决return null时，login页面难以捕获错误信息，这种方法最简单
                 // 参考：https://www.soinside.com/question/uhkkM6TGRbh3vjxt8YJ5QQ
+                //todo: update时会有问题，因为当update获取 user信息时，我这里是没有添加获取用户信息的接口，而我是放到登录页面的，所以会有问题，这里需要登录接口
+                // 参考：https://next-auth.js.org/getting-started/client#updating-the-session
                 if (credentials.session) {
                     return JSON.parse(credentials.session)
                 }
