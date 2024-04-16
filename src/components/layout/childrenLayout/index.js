3505a0e1dfbc5ef77bfa8e7215a38a4a6d297b73
张威 <zhangwei@192.168.0.102>
2024-04-16 21:09:38 +0800

    update

--- src/components/layout/childrenLayout/index.js
+++ src/components/layout/childrenLayout/index.js
@@ -1,8 +1,8 @@
 'use client'
-import {useEffect, useState} from "react";
-import {ExclamationCircleFilled,LoginOutlined,LoadingOutlined} from "@ant-design/icons";
+import {useCallback, useEffect, useState} from "react";
+import {ExclamationCircleFilled, LoginOutlined, LoadingOutlined} from "@ant-design/icons";
 import dayjs from "dayjs";
-import { useSession } from "next-auth/react"
+import {useSession} from "next-auth/react"
 import Loading from "@/components/loading";
 import TopMenus from "@/components/layout/topMenus";
 import {refreshToken} from '@/lib/slices/authSlice'
@@ -19,7 +19,7 @@ dayjs.extend(utc)
 dayjs.extend(customParseFormat)
 dayjs.locale('zh-cn') // 全局使用
 export default function ChildrenLayout({children}) {
-    const { data: session, status } = useSession()
+    const {data: session, status} = useSession()
     const dispatch = useDispatch()
 
     const [isOnline, setOnline] = useState(true)
@@ -30,46 +30,62 @@ export default function ChildrenLayout({children}) {
 
     // 注意：因为我们使用了refetchOnWindowFocus={true}，所以当页面显示的时候就会重新调用session，因此，这里就会被调用，
     // 也就是说相当于添加了document.addEventListener('visibilitychange'事件，利用这点来添加自定义逻辑
-    useEffect(() => {
-        console.log('session status=', status)
-        console.log('children session =', session)
-        if (session){
-            // tokenHandler()
-        }
-    }, [session])
+    // useEffect(() => {
+    //     console.log('session status=', status)
+    //     console.log('children session =', session)
+    //     const tokenHandlerWrapper = async () => {
+    //         if (session) {
+    //             await tokenHandler();
+    //         }
+    //     };
+    //
+    //     tokenHandlerWrapper();
+    //
+    //     // if (session){
+    //     //     tokenHandler()
+    //     // }
+    //     return () => {
+    //         tokenHandler()
+    //     }
+    // }, [session])
 
     useEffect(() => {
+        document.addEventListener('visibilitychange', tokenHandler);
+
         window.addEventListener("offline", changeOffline);
         window.addEventListener('online', changeOnline);
-
         return () => {
+            document.addEventListener('visibilitychange', tokenHandler);
+
             window.addEventListener("offline", changeOffline);
             window.addEventListener('online', changeOnline);
         };
     }, []);
 
-    async function tokenHandler() {
-        //todo:死循环了
-        const res = await dispatch(refreshToken())
-        console.log('tokenHandler refreshToken res=', res)
-        if (res.payload.status === 40001) {
-            confirm({
-                title: '登录已过期',
-                icon: <ExclamationCircleFilled/>,
-                content: '您的登录已过期，请重新登录！',
-                okText: '确定',
-                cancelText: '取消',
-                onOk() {
-                    location.href = '/login'
-                },
-                onCancel() {
-                    //弹出确认框，让用户自行跳转登录
-                    // 重新登录
-                    location.reload()
-                },
-            });
+    const tokenHandler = useCallback(async () => {
+        if (document.visibilityState === 'visible'){
+            //todo:死循环了
+            const res = await dispatch(refreshToken())
+            console.log('tokenHandler refreshToken res=', res)
+            // if (res.payload.status === 40001) {
+            //     confirm({
+            //         title: '登录已过期',
+            //         icon: <ExclamationCircleFilled/>,
+            //         content: '您的登录已过期，请重新登录！',
+            //         okText: '确定',
+            //         cancelText: '取消',
+            //         onOk() {
+            //             location.href = '/login'
+            //         },
+            //         onCancel() {
+            //             //弹出确认框，让用户自行跳转登录
+            //             // 重新登录
+            //             location.reload()
+            //         },
+            //     });
+            // }
         }
-    }
+    }, [session])
 
     function changeOffline() {
         setOnline(false)
@@ -87,9 +103,9 @@ export default function ChildrenLayout({children}) {
     }
 
     function renderChildren(children) {
-        if (status === 'loading'){
+        if (status === 'loading') {
             return <div className={'flex items-center justify-center p-2'}>
-                <Loading />
+                <Loading/>
             </div>
         }
         return <div>
@@ -100,9 +116,9 @@ export default function ChildrenLayout({children}) {
 
     return <div>
 
-        {isOnline?renderChildren(children):<div className={'flex flex-col justify-center items-center gap-4 pt-4'}>
+        {isOnline ? renderChildren(children) : <div className={'flex flex-col justify-center items-center gap-4 pt-4'}>
             {/*<Image width={200} height={200} src={lostnet}/>*/}
-            <LoadingOutlined className={'text-3xl'}/>
+            <LoadingOutlined className={'text-3xl py-4'}/>
             <div className={'text-2xl'}>连接到互联网</div>
             <div className={'text-base'}>你没有联网，请检查网络连接。</div>
         </div>}
